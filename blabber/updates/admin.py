8e16b84e9e2f3b51d5f70fe17e22ef8dd0a7756d
Kathleen Rauh <krauh2010@gmail.com>
2015-10-06 15:04:55 -0400

    More changes.

--- blabber/updates/admin.py
+++ blabber/updates/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Status, Favorite.
+from .models import Status, Favorite
 
 class StatusAdmin(admin.ModelAdmin):
     list_display = ['user', 'text', 'posted_at']
--- blabber/updates/models.py
+++ blabber/updates/models.py
@@ -4,15 +4,14 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
-from django.db import models
-
-# Create your models here.
-
-
 class Status(models.Model):
     #  creating columns in our model's data structure. Status is a subclass of Model.
-    text = models.Charfield(max_length=141)
-    posted_at = models.DateTime()
+    text = models.CharField(max_length=141)
+    posted_at = models.DateTimeField()
     user = models.ForeignKey(User)  # points to other tables to get data, using IDs.
                                     #  Id is implied for Status.)
-        
+
+
+class Favorite(models.Model):
+    user = models.ForeignKey(User)
+    Status = models.ForeignKey(Status)
